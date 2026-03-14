23edeb84e96e7039a130a18dff11a9bc6eafb7e2
EgbertSackey <kingegbertlloyd@gmail.com>
2026-03-14 01:31:59 -0400

    Create e1_data_types.c

---
+++ module2/e1_data_types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int age = 21;
+    float height = 1.8f;
+    double constant = 3.4147;
+    char fav_initial = 'S';
+    printf("Age: %d\n", age);
+    printf("Height: %f\n", height);
+    printf("Constant: %lf\n", constant);
+    printf("Favourite initial: %c\n", fav_initial);
+    return 0;
+}
