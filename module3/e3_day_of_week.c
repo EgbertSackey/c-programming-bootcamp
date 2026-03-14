14dcef608665e1cd289a84b3227eac40738a7116
EgbertSackey <kingegbertlloyd@gmail.com>
2026-03-14 01:38:29 -0400

    Create e3_day_of_week.c

---
+++ module3/e3_day_of_week.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    int day_num;
+    printf("Enter day number: ");
+    scanf("%d", &day_num);
+    switch (day_num) {
+        case 1:
+            printf("Monday\n");
+            break;
+        case 2:
+            printf("Tuesday\n");
+            break;
+        case 3:
+            printf("Wednesday\n");
+            break;
+        case 4:
+            printf("Thursday\n");
+            break;
+        case 5:
+            printf("Friday\n");
+            break;
+        case 6:
+            printf("Saturday\n");
+            break;
+        case 7:
+            printf("Sunday\n");
+            break;
+        default:
+            printf("Wanna speak with the romans to invent a new day?\n");
+    }
+    return 0;
+}
