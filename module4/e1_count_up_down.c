c1e9250f07cf028a6e9f8ee685f34156832bf09c
EgbertSackey <kingegbertlloyd@gmail.com>
2026-03-14 01:42:22 -0400

    Create e1_count_up_down.c

---
+++ module4/e1_count_up_down.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i <= 10; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+    for (int j = 10; j >= 1; j--) {
+        printf("%d ", j);
+    }
+    printf("\n");
+    return 0;
+}
