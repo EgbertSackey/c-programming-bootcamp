3da040fd2dcd6edbe43c5b2f42c6b131f3797d01
EgbertSackey <kingegbertlloyd@gmail.com>
2026-03-14 01:34:50 -0400

    Create e5_even_odd.c

---
+++ module2/e5_even_odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    printf("Please enter a number:\n");
+    int num = 0;
+    scanf("%d", &num);
+    if (num % 2 == 0) {
+        printf("Even\n");
+    } else {
+        printf("Odd\n");
+    }
+    return 0;
+}
